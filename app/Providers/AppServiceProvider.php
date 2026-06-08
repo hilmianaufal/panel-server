c0ee66cf06564e5a1b0d48d87a65def1165fc0e7
hilmianaufal <hilmiannaufal72@gmail.com>
2026-06-08 23:07:46 +0700

    Fix settings query before migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,24 @@
 
 namespace App\Providers;
 
+use App\Models\Setting;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        if (Schema::hasTable('settings')) {
+            View::share('setting', Setting::first());
+        } else {
+            View::share('setting', null);
+        }
     }
 }
